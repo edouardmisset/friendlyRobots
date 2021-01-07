7be42a9e49953572944d984da367afa484f2da70
Edouard Misset <edouardmisset@gmail.com>
2021-01-07 10:52:05 +0100

    First working react app

---
+++ src/Robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Oscar Wild",
+    username: "Oscario",
+    email: "oscario@la.eu",
+  },
+  {
+    id: 2,
+    name: "Pablo Picaso",
+    username: "Pablio",
+    email: "pablio@art.com",
+  },
+  {
+    id: 3,
+    name: "Totoro",
+    username: "Totoro",
+    email: "totoro@ghibli.jp",
+  },
+  {
+    id: 4,
+    name: "Miss Manhattan",
+    username: "Womanhattan",
+    email: "womanhattan@paris.fr",
+  },
+  {
+    id: 5,
+    name: "Mr Manhattan",
+    username: "Man-hattan",
+    email: "Manhattan@paris.fr",
+  },
+  {
+    id: 6,
+    name: "Boozy",
+    username: "Booziest",
+    email: "booziest@london.co.uk",
+  },
+  {
+    id: 7,
+    name: "Itwasgood Whileitlasted",
+    username: "IW",
+    email: "ttwasgoodwhileitlasted@oldtimes.info",
+  },
+];
---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      SearchField: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ SearchField: event.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  render() {
+    const { robots, SearchField } = this.state;
+    const filteredRobots = robots.filter((robots) => {
+      return robots.name.toLowerCase().includes(SearchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1 className="tc">Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Friendly Robots</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
