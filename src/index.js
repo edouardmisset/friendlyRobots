58aec3200b4ebd7e2941992626d920dbb769ce10
Edouard Misset <edouardmisset@gmail.com>
2021-09-03 18:48:58 +0200

    Added Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./containers/App";
-import reportWebVitals from "./reportWebVitals";
-import "tachyons";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import App from './containers/App'
+import reportWebVitals from './reportWebVitals'
+import 'tachyons'
+import { searchRobots } from './reducer'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchRobots)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
