e36ab16ec07d9e550bd3ad9c3d06e3c7fae22d2a
Edouard Misset <edouardmisset@gmail.com>
2021-09-04 11:36:48 +0200

    Added the connect to the App.js (with mapStateToProps & mapDispatchToProps)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,21 @@
 import { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary'
+import { setSearchField } from '../actions'
 
-export default function App() {
+const mapStateToProps = state => ({
+  searchField: state.searchField,
+})
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: event => dispatch(setSearchField(event.target.value)),
+})
+
+function App() {
   const [robots, setRobots] = useState([])
   const [searchField, setSearchField] = useState('')
 
@@ -37,3 +47,5 @@ export default function App() {
     <h1 className="tc">Loading...</h1>
   )
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
