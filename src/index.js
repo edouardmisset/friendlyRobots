29c193fb3c39024a60719db7071c713aae9011f7
Edouard Misset <edouardmisset@gmail.com>
2021-11-08 11:51:18 +0100

    WIP - Redux Middleware / logger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import './index.css'
-import App from './containers/App'
-import reportWebVitals from './reportWebVitals'
+import { applyMiddleware, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
 import 'tachyons'
+import App from './containers/App'
+import './index.css'
 import { searchRobots } from './reducer'
+import reportWebVitals from './reportWebVitals'
 
-const store = createStore(searchRobots)
+const logger = createLogger()
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store}>
