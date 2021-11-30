dfa6442ccd1c4494592e02ab3d80df71f2f0cdca
Edouard Misset <edouardmisset@gmail.com>
2021-11-30 11:52:25 +0100

    Small refactor

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,11 @@
-const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robot' src={`https://robohash.org/${id}?400x400`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+const Card = ({ name, email, id }) => (
+  <div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <img alt='robot' src={`https://robohash.org/${id}?400x400`} />
+    <div>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,11 @@
 import Card from './Card';
 
-const CardList = ({ robots }) => {
-  return (
-    <div>
-      {robots.map(({ id, email, name }) => (
-        <Card key={id} id={id} name={name} email={email} />
-      ))}
-    </div>
-  );
-};
+const CardList = ({ robots }) => (
+  <div>
+    {robots.map(({ id, email, name }) => (
+      <Card key={id} id={id} name={name} email={email} />
+    ))}
+  </div>
+);
 
 export default CardList;
